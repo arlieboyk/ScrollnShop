aea359c5767d882822f5425c96cdeaa905a41b52
Arlie <arlieboy.41@gmail.com>
2023-03-20 20:01:46 +0800

    first commit

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,12 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export async function GET(request: NextApiRequest, res: NextApiResponse) {
+  const user = await prisma.user.findMany()
+  return res.status(200).json(user)
 }
+
+
+
